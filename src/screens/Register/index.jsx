749485befc03a36736d75b7fe9bbf955d01c6a52
aleksadroww <aleksandroww97@gmail.com>
2020-08-06 18:49:59 +0300

    Register Validations Form.

--- src/screens/Register/index.jsx
+++ src/screens/Register/index.jsx
@@ -1,28 +1,70 @@
-import React from 'react';
-//import styles from './index.module.css';
+import React, { useState } from 'react';
+// import styles from './index.module.css';
+import { useForm } from 'react-hook-form';
 
-//TO DO validation of password and username
+const Register = () => {
+  const { handleSubmit, register, errors } = useForm();
+  const onSubmit = (values) => console.log(values);
+  const [password, setPassword] = useState(0);
+  console.log(useState);
 
-function Register() {
-    return (
-        <form className="Register">
-            <div className="form-control">
-                <label>Username</label>
-                <input type="text"/>
-                </div>
-            <div className="form-control">
-                <label>Password</label>
-                <input type="password"/>
-               </div>
-            <div className="form-control">
-                <label>Re-Password</label>
-                <input type="password"/>
-            </div>
-            <div className="form-control">
-                <button type="button">Register</button>
-            </div>
-        </form>
-    )
-}
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <label>Email: </label>
+      <input
+        name="email"
+        ref={register({
+          required: 'Required',
+          pattern: {
+            value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+            message: 'Invalid email address!',
+          },
+        })}
+      />
+      {errors.email && errors.email.message}
+
+      <label>Username: </label>
+      <input
+        name="username"
+        ref={register({
+          required: 'Required',
+          pattern: {
+            value: /^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/i,
+            message: 'Invalid username!',
+          },
+        })}
+      />
+      {errors.username && errors.username.message}
+
+      <label>Password</label>
+      <input
+        onChange={(e) => setPassword(e.target.value)}
+        name="password"
+        type="password"
+        ref={register({
+          required: 'Required',
+          pattern: {
+            value: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/i,
+            message:
+              'Minimum eight characters, at least one letter and one number!',
+          },
+        })}
+      />
+      {errors.password && errors.password.message}
+
+      <label>Re-Password</label>
+      <input
+        name="rePassword"
+        type="rePassword"
+        ref={register({
+          validate: (value) => value === password,
+        })}
+      />
+      {errors.rePassword && errors.rePassword.message}
+
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
 
 export default Register;
